215039dfc9849c5b3bed512804ad2bdea29a8959
magdalenacabaj <mc.cabaj@gmail.com>
2015-10-23 17:18:33 +0200

    add admin class

--- src/admin.php
+++ src/admin.php
@@ -1,28 +1,40 @@
 <?php
 
-class Admin{
+/*
+CREATE TABLE admins(
+    admin_id INT AUTO_INCREMENT,
+    email VARCHAR(60) UNIQUE,
+    password CHAR(60),
+    PRIMARY KEY(admin_id)
+)
+*/
+
+class Admin
+{
     static private $conn;
 
     private $id;
-    private $userName;
+    private $name;
     private $email;
-    private $description;
+    private $password;
 
-    public static function setConnection(mysqli $newConnection){
+    public static function setConnection(mysqli $newConnection)
+    {
         self::$conn = $newConnection;
     }
 
-    static public function LogIn($email, $password){
-        $sql = "SELECT * FROM users WHERE  email = '$email'";
+    static public function LogIn($email, $password)
+    {
+        $sql = "SELECT * FROM admins WHERE  email = '$email'";
         $result = self::$conn->query($sql);
 
-        if ($result == true){
-            if($result->num_rows ==1){
+        if ($result == true) {
+            if ($result->num_rows == 1) {
                 $row = $result->fetch_assoc();
 
-                if(password_verify($password, $row["password"])){
-                    $loggedUser = new User($row["user_id"], $row["email"], $row["description"]);
-                    return $loggedUser;
+                if (password_verify($password, $row["password"])) {
+                    $loggedAdmin = new Admin($row["admin_id"], $row["email"], $row["name"]);
+                    return $loggedAdmin;
                 }
 
             }
@@ -30,140 +42,87 @@ class Admin{
         return false;
     }
 
-    static public function register($newEmail, $password, $password2, $newDescription){
-        if($password != $password2){
-            return false;
-        }
 
-        $hasshedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-        $sql= "INSERT INTO users (email, password, description) VALUES ('$newEmail', '$hasshedPassword', '$newDescription')";
-        $result = self::$conn->query($sql);
-
-        if(self::$conn == true){
-            $newUser = new User(self::$db->insert_id, $newEmail, $newDescription);
-            return $newUser;
-
-        }
-        echo self::$conn->error;
-        return false;
-    }
-
-    public function __construct($newId, $newEmail, $newDescription){
-        $this->id=$newId;
+    public function __construct($newId, $newName, $newEmail)
+    {
+        $this->id = $newId;
         $this->email = $newEmail;
-        $this->setDescription($newDescription);
+        $this->name = $newName;
+
     }
 
 
-    public function saveToDB(){
-        $sql = "UPDATE users SET description = '{$this->description}' WHERE user_id='{$this->id}'";
-        $result=self::$conn->query($sql);
-        return $result;
-    }
-
-    static public function getUserById($id){
-        $sql = "SELECT * FROM users WHERE user_id = ($id)";
+    static public function getAdminById($id)
+    {
+        $sql = "SELECT * FROM admmins WHERE admin_id = ($id)";
         $result = self::$conn->query($sql);
-        if($result == true) {
+        if ($result == true) {
             if ($result->num_rows == 1) {
                 $row = $result->fetch_assoc();
-                $loggedUser = new User($row['user'], $row['email'], $row['description']);
+                $loggedUser = new Admin($row['name'], $row['email']);
                 return $loggedUser;
             }
         }
         return false;
     }
 
-    static public function getAllUsers(){
-        $ret=[];
-        $sql = "SELECT * FROM users";
-        $result= self::$conn->query($sql);
-        if($result == true){
-            if($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $loadedUser = new User($row['user_id'], $row['email'], $row['description']);
-                    $ret[] = $loadedUser;
-                }
-            }
-
-        }
-
-        return $ret;
-    }
-
-    public function loadUserTweets($userId){
-
-        $ret = [];
-
-        $sql = "SELECT * FROM tweets WHERE user_id = $userId";
-        $result = self::$conn->query($sql);
-        if($result == true){
-            if($result->num_rows >0){
-                while($row = $result->fetch_assoc()){
-                    $userTweets = new Tweet($row['tweet_id'], $row['user_id'], $row['creation_date'], $row['text']);
-                    $ret[] = $userTweets;
-                }
+    public static function createMessage($id, $text){
+        if(is_string($text) && strlen($text) <= 140){
+            $sql = "INSERT INTO tweets(text, creation_date, user_id) VALUES ('$text', NOW(), '$userId')";
+            $result = self::$conn->query($sql);
+            if($result == true){
+                $myTweet = new Tweet(self::$conn->insert_id, $userId, date("Y-m-d H:i:s"), $text);
+                return $myTweet;
             }
         }
-        return $ret;
-    }
-
-
-
-    ///////////////////
-
-    public function createTweet($tweetText){
+        return false;
 
     }
 
-    public function getAllTweets(){
-        $ret=[];
-        return $ret;
-        //after implementing tweet add
+
+    public function getName()
+    {
+        return $this->name;
     }
 
 
-
-
-    public function getAllComments(){
-        $ret=[];
-        return $ret;
-        //after implementing tweet add
+    public function setName($name)
+    {
+        $this->name = $name;
     }
-    public function createComment(){
-        $ret=[];
-        return $ret;
-        //after implementing tweet add
-    }
-    //////////////////////////
 
     public function getId()
     {
         return $this->id;
     }
 
-    public function setUserName($newUserName){
-        if(is_string($newUserName) && strlen($newUserName)<60){
-            $this->userName = $newUserName;
-        }
-    }
 
-    public function getUserName(){
-        return $this->userName;
-    }
-    public function getEmail(){
+
+    public function getEmail()
+    {
         return $this->email;
-
     }
 
-    public function getDescription(){
-        return $this->description;
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 
-    public function setDescription($newDescription){
-        if(is_string($newDescription) && strlen($newDescription)<255){
-            return $this->description = $newDescription;
-        }
+
+    public function getPassword()
+    {
+        return $this->password;
     }
+
+
+
+
+
+
+
+
+
 }
+
+
