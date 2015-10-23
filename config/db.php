a95ec0ba4d96103ef850e3db8dfcc5589f5bd040
magdalenacabaj <mc.cabaj@gmail.com>
2015-10-23 17:40:13 +0200

    -zmiany

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("src/admin.php");
+
+define('DB_HOST','localhost');
+define('DB_PORT','8888');
+define('DB_USER','root');
+define('DB_PASSWORD', 'coderslab');
+define('DB_DB', 'shop');
+
+define('BASE_PATH', '/sklepint');
+
---
+++ routing.php
@@ -0,0 +1,5 @@
+<?php
+
+$router->map('GET', '/rejestracja/', 'register.php');
+$router->map('GET', '/logowanie/', 'login.php');
+//$router->map('GET', '/produkt[i:pobfjd]/', 'login.php');
